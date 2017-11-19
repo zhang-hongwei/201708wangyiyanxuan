f19166cffd5a697ba7f9ca71b67ff0edc04e93c2
zhanghongwei <zhang123910@live.com>
2017-11-20 00:52:00 +0800

    17-11-20 project init

---
+++ index.js
@@ -0,0 +1,26 @@
+let path = require("path")
+let htmlWebpackPlugin = require("html-webpack-plugin")
+module.exports = {
+    entry: "./src/index.js",//入口文件
+    output: {
+        path: path.resolve("build"),//输出的目录
+        filename: "bundle.js",//输出的文件名
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    module: {
+        //存放很多模块加载规则
+        rules: [
+            { test: /\.js$/, use: "babel-loader", exclude: /node_modules/ },
+            { test: /\.less/, use: ["style-loader", "css-loader", "less-loader"] },
+            { test: /\.(jpg|png|gif|svg)$/, use: "url-loader" }
+        ]
+    },
+    devtool: "cheap-module-source-map",//在出错的时候可以提示具体源文件的代码行数，而非bundle.js行数
+    plugins: [
+        new htmlWebpackPlugin({
+            template: "index.html"
+        })
+    ]
+}
--- src/components/Tab/tab.js
+++ src/components/Tab/tab.js
@@ -4,24 +4,24 @@ import {NavLink} from "react-router-dom"
 export default class Tab extends React.Component{
     render(){
         return(
-            <div className="tabs">
-                <NavLink exact to="/">
-                    <i className="iconfont icon-shouye"></i>
+            <div className="m-tabBar">
+                <NavLink className="item" exact to="/">
+                    <i className="iconfont icon-shouye4"></i>
                     <span>首页</span>
                 </NavLink>
-                <NavLink to="/identify">
-                    <i className="iconfont icon-nixiangyaozhao"></i>
+                <NavLink className="item" to="/identify">
+                    <i className="iconfont icon-zhengfangxing "></i>
                     <span>识物</span>
                 </NavLink>
-                <NavLink to="/classify">
-                    <i className="iconfont icon-fenlei2"></i>
+                <NavLink className="item" to="/classify">
+                    <i className="iconfont icon-fenlei3"></i>
                     <span>分类</span>
                 </NavLink>
-                <NavLink to="/cart">
-                    <i className="iconfont icon-gouwuche"></i>
+                <NavLink className="item" to="/cart">
+                    <i className="iconfont icon-icon-shoppingcart-02"></i>
                     <span>购物车</span>
                 </NavLink>
-                <NavLink to="/profile">
+                <NavLink className="item" to="/profile">
                     <i className="iconfont icon-geren"></i>
                     <span>个人</span>
                 </NavLink>
